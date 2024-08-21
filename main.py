afed5d3ee8cac12ac8d2dbeb128308f0ec4ddf22
harrylew25 <harrylew25@gmail.com>
2024-08-22 00:29:58 +0800

    Limit the game to 60 fps

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def main():
     # print(f'Screen width: {SCREEN_WIDTH}')
     # print(f'Screen height: {SCREEN_HEIGHT}')
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
 
         # This checks if user has closed the window and exit the game loop if they do.
@@ -20,6 +24,9 @@ def main():
 
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
         
 
 if __name__ == '__main__':
