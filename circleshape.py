bfb80b2d246e3f4a2ced1a667914d98a4a82dd59
harrylew25 <harrylew25@gmail.com>
2024-08-27 01:17:02 +0800

    Updated the base class to add draw, update and collides functions

--- circleshape.py
+++ circleshape.py
@@ -3,7 +3,7 @@ import pygame
 #Base class for game objects
 class CircleShape(pygame.sprite.Sprite):
     def __init__(self, x, y, radius):
-        if hasattr(self, 'container'):
+        if hasattr(self, 'containers'):
             super().__init__(self.containers)
         else:
             super().__init__()
@@ -13,7 +13,12 @@ class CircleShape(pygame.sprite.Sprite):
         self.radius  = radius
 
     def draw(self, screen):
+        # must override
         pass
 
     def update(self, dt):
+        # must override
         pass
+
+    def collides_with(self, other):
+        return self.position.distance_to(other.position) <= self.radius + other.radius
